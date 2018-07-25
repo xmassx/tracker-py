ee5eaf87ef395079212d3921817ca672b5affd96
xmass <xmassx1989@gmail.com>
2018-07-26 00:07:23 +0300

    add basic sqlite support with dirty impl

--- tracker.py
+++ tracker.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from time import sleep
 from pprint import pprint
+import time
+import sqlite3
+
 
 class abstract_activity(object):
   """
@@ -30,22 +33,24 @@ class abstract_activity(object):
       return "Programming"
     else:
       return "Home"
-    
+  
   def start_activity(self):
     if self.status == 'stop':
-      print("started", self.id)
+      # print("started", self.id)
       self.status = 'run'
       self.current_active.update(self)
     else:
-      print("already start", self.id)
+      # print("already start", self.id)
+      pass
   
   def stop_activity(self):
     if self.status == 'run':
-      print('stopped', self.id)
+      # print('stopped', self.id)
       self.status = 'stop'
       self.current_active.update(self)
     else:
-      print('already stopped', self.id)
+      # print('already stopped', self.id)
+      pass
 
 class current_activity(object):
   """
@@ -53,7 +58,7 @@ class current_activity(object):
   """
   
   def __init__(self, activity=None, dbo=None):
-    print("init current active")
+    # print("init current active")
     if activity:
       self.activity = activity
       self.started = self.get_time()
@@ -67,7 +72,9 @@ class current_activity(object):
       self.dbo = dbo
   
   def get_time(self):
-    return datetime.utcnow()
+    # a=time.time()
+    # return datetime.utcfromtimestamp(a)
+    return time.time()
   
   def update(self, activity):
     current_date = self.get_time()
@@ -75,45 +82,87 @@ class current_activity(object):
       self.stopped = current_date
       self.activity.status = 'stop'
       self.write_stop_to_dbo()
-      
+    
     self.activity = activity
     self.started = current_date
     self.write_start_to_dbo()
-    
+  
   def write_start_to_dbo(self):
-    curr_dict = {'id': self.activity.id, 'state': self.activity.status, 'date': self.started, 'from': 'start'}
-    self.dbo.append(curr_dict)
-    
+    # curr_dict = {'id': self.activity.id, 'state': self.activity.status, 'date': self.started}
+    # self.dbo.append(curr_dict)
+    curr_tuple = (self.activity.id, self.activity.status, self.started)
+    self.dbo.append_tuple(curr_tuple)
+  
   def write_stop_to_dbo(self):
-    curr_dict = {'id': self.activity.id, 'state': self.activity.status, 'date': self.stopped, 'from': 'stop'}
-    self.dbo.append(curr_dict)
+    # curr_dict = {'id': self.activity.id, 'state': self.activity.status, 'date': self.stopped}
+    # self.dbo.append(curr_dict)
+    curr_tuple = (self.activity.id, self.activity.status, self.stopped)
+    self.dbo.append_tuple(curr_tuple)
+
 
 class DBO(object):
   """
   abstract over database
   """
   
-  def __init__(self):
-    self.db = []
+  def __init__(self, dbs=':memory:'):
+    try:
+      create_db = """CREATE TABLE tracker(n INTEGER PRIMARY KEY AUTOINCREMENT,
+      id INTEGER, state TEXT, date REAL)"""
+      self.INSERT_TEMPLATE = 'INSERT INTO tracker(id, state, date) VALUES(?, ?, ?)'
+      self.db = sqlite3.connect(dbs)
+      self.cursor = self.db.cursor()
+      self.cursor.execute(create_db)
+      self.db.commit()
+    except Exception as e:
+      print(e)
+    # self.db = []
   
   def append(self, d={}):
     self.db.append(d)
+  
+  def append_tuple(self, d=tuple):
+    try:
+      # print('Hi i have {}'.format(d))
+      self.cursor.execute(self.INSERT_TEMPLATE, d)
+      self.db.commit()
+    except Exception as e:
+      print(e)
+  
+  def print(self):
+    try:
+      self.cursor.execute('SELECT n, id, state, date FROM tracker')
+      fetched = self.cursor.fetchall()
+      for row in fetched:
+        print(row)
+    except Exception as e:
+      print(e)
+    return None
+    
 
 
 def main():
-  db = DBO()
+  # db_file = ':memory:'
+  db_file = 'test.sqlite'
+  db = DBO(db_file)
   curr_activ = current_activity(None, dbo=db)
   a1 = abstract_activity('a', curr_activ)
   c1 = abstract_activity('c', curr_activ)
-  a1.start_activity()
-  c1.start_activity()
-  a1.stop_activity()
-  c1.stop_activity()
-  a1.stop_activity()
-  a1.start_activity()
-  a1.stop_activity()
-  #print(a1.type, c1.id, a1.status)
-  pprint(db.db)
+  a = time.time()
+  for i in range(1000):
+    a1.start_activity()
+    c1.start_activity()
+    a1.stop_activity()
+    c1.stop_activity()
+    a1.stop_activity()
+    a1.start_activity()
+    a1.stop_activity()
+    print('{} gone'.format(i))
+  # print(a1.type, c1.id, a1.status)
+  # pprint(db.db)
+  db.print()
+  b = time.time()
+  print(b-a)
   
 if __name__ == "__main__":
   main()
